d4126fa33f7ead919e9df1bc18b520d5660921fd
enicio <jessandro42@gmail.com>
2021-08-14 18:56:08 -0300

    Configurado multer com upload para pasta local

--- src/controllers/assetController.js
+++ src/controllers/assetController.js
@@ -4,7 +4,13 @@ const CREATE_STATUS = 201;
 const RESPONSE_STATUS = 200;
 
 async function createAsset(req, res) {
-  const asset = req.body;
+  let { dados } = req.body;
+  console.log(req.file);
+  const { key, destination } = req.file;
+  // console.log(req.imagePath);
+  let asset  = JSON.parse(dados);
+  asset = {...asset, image: `${destination}${key}` };
+  // console.log(asset);
   const result = await assetService.createAsset(asset);
   console.log('on controller', result);
   res.status(CREATE_STATUS).send(result);
